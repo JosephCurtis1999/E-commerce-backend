2463f685eb1fccbd6e5e74efd36741a52f295cdf
Joe Curtis <joecurtis9999@gmail.com>
2022-03-29 12:54:07 +0100

    fixed find all tags function

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -14,7 +14,7 @@ router.get('/', (req, res) => {
       },
     ]
   })
-  .then(dbProductData => res.json(dbProductData))
+  .then(dbTagdata => res.json(dbTagdata))
   .catch(err=>{
     console.log(err);
     res.status(500).json(err);
@@ -24,6 +24,18 @@ router.get('/', (req, res) => {
 router.get('/:id', (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  Tag.findOne({
+    where: {
+      id:req.params.id
+    },
+    include: [
+      {
+        model: Product,
+        attributes: ['id', 'product_name', 'price', 'stock', 'category_id']
+      }
+    ]
+  })
+  .then(dbTagdata)
 });
 
 router.post('/', (req, res) => {
