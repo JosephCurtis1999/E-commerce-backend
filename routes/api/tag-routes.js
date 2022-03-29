08ec472d982ab8d34bea85ec963b1221036c8482
Joe Curtis <joecurtis9999@gmail.com>
2022-03-29 13:00:23 +0100

    update a tag's name function using update

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -62,6 +62,22 @@ router.post('/', (req, res) => {
 
 router.put('/:id', (req, res) => {
   // update a tag's name by its `id` value
+  Tag.update(req.body, {
+    where: {
+      id:req.params.id
+    },
+  })
+  .then(dbTagdata => {
+    if (!dbTagdata[0]) {
+      res.status(404).json({ message: 'No tag gound with this id'});
+      return;
+    }
+    res.json(dbTagdata);
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 router.delete('/:id', (req, res) => {
